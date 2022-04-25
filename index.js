1dca96734f56cd589e3d26dbac6efbaae5000805
milkoqq <theofanous.dim@gmail.com>
2022-04-25 23:12:34 +0300

    1 file modified

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-console.log('hello');
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
 
-console.log('did you receive the update asshole?');
+Note:
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
