9ac889b8ffdd26a41bf7abc9b415969816bf9b57
milkoqq <theofanous.dim@gmail.com>
2022-04-25 22:17:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vcawvp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vcawvp)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+console.log('hello');
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
